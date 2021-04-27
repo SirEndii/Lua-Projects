95a0b39c70cf985a0e3a17c4f4aa145888f57e9b
Sr_endi <info@denizdemiroglu.de>
2021-04-27 18:46:44 +0200

    Just a commit

--- src/ME Cpus/startup.lua
+++ src/ME Cpus/startup.lua
@@ -23,15 +23,15 @@ os.loadAPI("bars")
 
 function prepareMon()
     mon.clear()
+    monX, monY = mon.getSize()
     mon.setBackgroundColor(colors.black)
     mon.setCursorPos(13,1)
     mon.setTextScale(1)
     mon.write("ME Crafting CPUs")
     mon.setTextScale(1)
     mon.setCursorPos(1,1)
-    monX, monY = mon.getSize()
-    drawBox(2, 38, 3, 16, "CPU's", colors.gray, colors.lightGray)
-    drawBox(2, 38, 18, 25, "Stats", colors.gray, colors.lightGray)
+    drawBox(2, monX - 2, 3, monY - 8, "CPU's", colors.gray, colors.lightGray)
+    drawBox(2, monX - 2, monY - 6, monY - 1, "Stats", colors.gray, colors.lightGray)
     addBars()
 end
 
@@ -40,7 +40,7 @@ function addBars()
     for i=1, #cpus do
         x = 3*i
         full = (cpus[i].storage/65536) + cpus[i].coProcessors
-        bars.add(""..i,"ver", full, cpus[i].coProcessors, 1+x, 5, 2, 10, colors.purple, colors.lightBlue)
+        bars.add(""..i,"ver", full, cpus[i].coProcessors, 1+x, 5, 2, monY - 10, colors.purple, colors.lightBlue)
         mon.setCursorPos(x+1, 15)
         mon.write(string.format(i))
     end
@@ -101,7 +101,7 @@ function comma_value(n) -- credit http://richard.warburton.it
 end
 
 function updateStats()
-    clear(3,37,19,24)
+    clear(3,monX - 3,monY - 5,monY - 2)
     print("CPUs: ".. data.cpus)
     print("Working: ".. data.crafting)
     mon.setCursorPos(4,20)
@@ -119,7 +119,7 @@ function updateStats()
         mon.write(comma_value(data.bytes) .." | ".. comma_value(data.bytesUsed))
     end
     if tablelength(bars.getBars()) ~= data.cpus then
-        clear(3,37,4,15)
+        clear(3,monX - 3,4,monY - 8)
         bars.clear()
         addBars()
     end
