6968bc644aff629e91c5d6feadfd48fbca929fb3
Sr_endi <info@denizdemiroglu.de>
2021-05-02 15:41:14 +0200

    Removed emmylua stuff

--- src/installer.lua
+++ src/installer.lua
@@ -4,11 +4,7 @@
 --- DateTime: 02.05.2021 14:48
 ---
 
-github = "https://raw.githubusercontent.com/Seniorendi/Lua-Projects/master/"
-
-programs = {
-    mecpus = {link= github.."src/ME%20Cpus/startup.lua", name="ME Cpus"}
-}
+local githuburl = "https://raw.githubusercontent.com/Seniorendi/Lua-Projects/master/"
 
 local args = {...}
 
