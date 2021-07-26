288ea473977ff5229f85c5ba61aca52a0cd56626
Sr_endi <67484093+Seniorendi@users.noreply.github.com>
2021-07-26 15:30:05 +0200

    Create colony_integrator_status.lua

---
+++ Examples/colony_integrator_status.lua
@@ -0,0 +1,18 @@
+---
+--- Made for the Advanced Peripherals documentation
+--- Created by Srendi.
+--- DateTime: 25.04.2021 20:44
+--- Link: https://docs.srendi.de/peripherals/colony_integrator/
+---
+
+local colony = peripheral.wrap("back")
+ 
+print("Building Sites: ".. colony.amountOfConstructionSites())
+print("Citizens: ".. colony.amountOfCitizens())
+local underAttack = "No"
+if colony.isUnderAttack() then
+    underAttack = "Yes"
+end
+print("Is under attack? ".. underAttack)
+print("Overall happiness: ".. math.floor(colony.getHappiness()))
+print("Amount of graves: ".. colony.amountOfGraves())
