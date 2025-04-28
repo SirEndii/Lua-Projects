09df50427f4390ce1cda1a4bf19f88b1db2fef1e
Srendi <info@denizdemiroglu.de>
2025-04-28 22:44:58 +0200

    init commit for the new storage requester

---
+++ src/Storage Requester/storagerequester.lua
@@ -0,0 +1,7 @@
+---
+--- Made for the Advanced Peripherals documentation - Can be used in production
+--- Created by Srendi - https://github.com/SirEndii
+--- Link: https://docs.advanced-peripherals.de/0.7-bridges/guides/storage_system_functions/
+---
+
+print("womp")
